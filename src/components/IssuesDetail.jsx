33f7abb9d140a382edb13a39ca18cb18e7cb9d3d
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-08 14:55:05 +0530

    svg in sepearate in svg file

--- src/components/IssuesDetail.jsx
+++ src/components/IssuesDetail.jsx
@@ -1,16 +1,18 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import dot from "../assets/svg/dot.svg";
+import smiley from "../assets/svg/smiley.svg";
 
 export default function IssuesDetail() {
   const [singleIssue, setSingleIssue] = useState(null);
-  const [loading, setLoading] = useState("false");
+  const [loading, setLoading] = useState(false);
   const { org, repo, number } = useParams();
 
   useEffect(() => {
     async function fetchIssue() {
       try {
-        setLoading("true");
+        setLoading(true);
         const singleIssue = await Axios.get(
           `https://api.github.com/repos/${org}/${repo}/issues/${number}`
         );
@@ -25,7 +27,7 @@ export default function IssuesDetail() {
 
   return (
     <div>
-      {loading === "false" ? (
+      {loading === false ? (
         <h1>Issues Loading</h1>
       ) : loading === "null" ? (
         <h3>Something is wrong</h3>
@@ -56,12 +58,12 @@ export default function IssuesDetail() {
             <div className="single-issue-body border rounded ml-3">
               <div className="border-bottom d-flex justify-content-between p-2 rounded-top body-header">
                 <span>{singleIssue.user.login}</span>
-                <div>
+                <div className="d-flex">
                   <span className="px-2 display-media-none">
-                    <Smiley />
+                    <img src={smiley} alt="smiley" />
                   </span>
                   <span className="px-2">
-                    <Dot />
+                    <img src={dot} alt="dot" />
                   </span>
                 </div>
               </div>
@@ -76,31 +78,31 @@ export default function IssuesDetail() {
   );
 }
 
-function Smiley() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      width="16"
-      height="16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"
-      ></path>
-    </svg>
-  );
-}
+// function Smiley() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 16 16"
+//       width="16"
+//       height="16"
+//     >
+//       <path
+//         fillRule="evenodd"
+//         d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"
+//       ></path>
+//     </svg>
+//   );
+// }
 
-function Dot() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      width="16"
-      height="16"
-    >
-      <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-    </svg>
-  );
-}
+// function Dot() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 16 16"
+//       width="16"
+//       height="16"
+//     >
+//       <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+//     </svg>
+//   );
+// }
--- src/components/Issueslist.jsx
+++ src/components/Issueslist.jsx
@@ -2,16 +2,19 @@ import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { NavLink, useParams } from "react-router-dom";
+import openIssue from "../assets/svg/openissueicon.svg";
+import check from "../assets/svg/checkicon.svg";
+import comment from "../assets/svg/comment.svg";
 
 export default function IssuesList() {
   const [issues, setIssues] = useState(null);
-  const [loading, setLoading] = useState("false");
+  const [loading, setLoading] = useState(false);
   const { org, repo } = useParams();
 
   useEffect(() => {
     async function fetchIssuesList() {
       try {
-        setLoading("true");
+        setLoading(true);
         const issue = await Axios.get(
           `https://api.github.com/repos/${org}/${repo}/issues`
         );
@@ -27,15 +30,11 @@ export default function IssuesList() {
       <div className="border rounded">
         <div className="border-bottom d-flex justify-content-between p-3 first-row rounded-top text-small media-flex-around">
           <div className="d-flex display-media-none">
-            <div className="mr-2">
-              <p>
-                <OpenIssuesIcon /> <span>Open</span>
-              </p>
+            <div className="mr-2 d-flex">
+              <img src={openIssue} alt="openIssue" /> <span>Open</span>
             </div>
-            <div>
-              <p>
-                <Check /> <span>Closed</span>
-              </p>
+            <div className="d-flex">
+              <img src={check} alt="check" /> <span>Closed</span>
             </div>
           </div>
           <div className="d-flex ">
@@ -48,7 +47,7 @@ export default function IssuesList() {
           </div>
         </div>
         <ul>
-          {loading === "false" ? (
+          {loading === false ? (
             <h3>Issues is Loading..</h3>
           ) : loading === "null" ? (
             <h3>Something is wrong</h3>
@@ -58,8 +57,8 @@ export default function IssuesList() {
                 <li key={i} className="border-bottom">
                   <div className="d-flex justify-content-between">
                     <div className="d-flex">
-                      <div className=" pl-3 pt-2 text-success">
-                        <OpenIssuesIcon />
+                      <div className=" pl-3 pt-2 text-success flex-start">
+                        <Openissue />
                       </div>
                       <div className="p-2">
                         <div>
@@ -101,9 +100,9 @@ export default function IssuesList() {
                       </div>
                       <div className="px-4">
                         {issue.comments ? (
-                          <>
-                            <CommentIcon /> {issue.comments}
-                          </>
+                          <div>
+                            <img src={comment} alt="comment" /> {issue.comments}
+                          </div>
                         ) : (
                           ""
                         )}
@@ -122,7 +121,7 @@ export default function IssuesList() {
   );
 }
 
-function OpenIssuesIcon() {
+function Openissue() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -134,38 +133,6 @@ function OpenIssuesIcon() {
       <path
         fillRule="evenodd"
         d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"
-      ></path>
-    </svg>
-  );
-}
-
-function Check() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      width="16"
-      height="16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"
-      ></path>
-    </svg>
-  );
-}
-
-function CommentIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      width="16"
-      height="16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z"
       ></path>
     </svg>
   );
